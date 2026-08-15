71cd8370d5e65815ff5845ffac10775af3a1812a
ZAHIDHOSEN <zahidhosen203@gmail.com>
2026-08-15 22:07:01 +0600

    project setup completed with react typescript redux tookit

---
+++ src/Routes/Router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="bg-red-600">Hello World</div>,
+  },
+]);
