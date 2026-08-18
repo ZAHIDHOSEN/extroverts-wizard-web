b3cb4647e8ea2d2fec3af0f14e180aef6f74736f
ZAHIDHOSEN <zahidhosen203@gmail.com>
2026-08-18 23:30:43 +0600

    setup pages in routes

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Landing from "../pages/Landing/Landing";
+import Terms from "../pages/Terms/Terms";
+import SignupWizaed from "../pages/Signup/SignupWizaed";
 
 
 
@@ -8,4 +10,12 @@ export const router = createBrowserRouter([
     path: "/",
     element: <Landing></Landing>
   },
+  {
+    path: "/terms",
+    element: <Terms></Terms>
+  },
+  {
+    path: "/signup",
+    element: <SignupWizaed></SignupWizaed>
+  }
 ]);
--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { store } from './store/store';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
    </Provider>
   </StrictMode>,
 )
